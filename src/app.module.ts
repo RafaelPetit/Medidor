02e9d797a6f077ef5c115f21d65cd9c3dc44ea2b
Rafael Petit <rpetit.dev@gmail.com>
2024-08-28 13:11:47 -0300

    feat: adicionando middleware para validar entrada de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,26 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { UserController } from './user/user.controller';
-import { UserService } from './user/user.service';
-import { UserModule } from './user/user.module';
-import { UserRepository } from './user/repository/user.repository';
-import { MeasurementModule } from './measure/measure.module';
+import { MeasureRepository } from './measure/repository/measure.repository';
+import { MeasureModule } from './measure/measure.module';
+import { PrismaModule } from './misc/prisma/prisma.module';
+import { AppController } from './app.controller';
 import { GeminiModule } from './gemini/gemini.module';
-import { ValidatorModule } from './misc/validator/validator.module';
+import { MapperModule } from './misc/mapper/mapper.module';
+import { MeasureValidationMiddleware } from './misc/middleware/measure-validate.middleware';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    UserModule,
-    MeasurementModule,
+    PrismaModule,
     GeminiModule,
-    ValidatorModule,
+    MapperModule,
+    MeasureModule,
   ],
-  controllers: [UserController],
-  providers: [UserRepository, GeminiModule],
+  controllers: [AppController],
+  providers: [MeasureRepository],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(MeasureValidationMiddleware).forRoutes('upload'); // Ajuste conforme necessário
+  }
+}
